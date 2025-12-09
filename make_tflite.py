cba478613118befb5e119a541b7d9e495b741496
JonJaryongLee <director9303@gmail.com>
2025-12-09 17:25:29 +0900

    수정

--- make_tflite.py
+++ make_tflite.py
@@ -19,7 +19,7 @@ loaded_model = torchvision.models.mobilenet_v3_small(
 loaded_model.classifier[3] = nn.Linear(in_features=loaded_model.classifier[3].in_features, out_features=10)
 
 MODEL_STATE_DICT_DIR_PATH = "./models_state_dict"
-FILE_NAME = "20251024_162854_mobilenet_v3_small_cifar10_finetuned.pth"
+FILE_NAME = "20251209_082006_mobilenet_v3_small_cifar10_finetuned.pth"
 FULL_PATH = os.path.join(MODEL_STATE_DICT_DIR_PATH, FILE_NAME)
 
 # 모든 state 를 cpu 로 로드하기
